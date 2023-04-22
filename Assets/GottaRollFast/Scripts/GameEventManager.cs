d722cea581f5b1564d02d1f0a809b952271c667e
Yuri1427934 <halo010354@gmail.com>
2023-04-22 11:35:39 -0600

    RollGame In

---
+++ Assets/GottaRollFast/Scripts/GameEventManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventManager : MonoBehaviour
+{
+    public static GameEventManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public UnityEvent<bool> ChargeLeg;
+
+    public UnityEvent StageClear;
+}
